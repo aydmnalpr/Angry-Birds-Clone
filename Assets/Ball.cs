8f816946bc12b1c6c1d13c5e84706e83aba1ca7f
Alper Ayduman <aydumanalper@gmail.com>
2020-07-10 11:54:46 +0300

    mechanic

---
+++ Assets/Ball.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private bool isPressed = false;
+    public float releaseTime = .15f;
+
+    public Rigidbody2D hook;
+    public float maxDragDistance = 2f;
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if(Vector3.Distance(mousePos, hook.position) > maxDragDistance)
+            {
+                rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
+            }
+            else
+            {
+                rb.position = mousePos;
+            }
+                
+        }
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+
+        this.enabled = false;
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.relativeVelocity.magnitude > 2)
+        {
+            Die();
+        }
+
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
